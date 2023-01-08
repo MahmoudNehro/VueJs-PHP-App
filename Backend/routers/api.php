a6c9d3c7c0f76992bbf0dbf4deabc32cd0d6325b
MahmoudNehro <mahmoudnehro9000@gmail.com>
2023-01-09 00:54:57 +0200

    commit

--- Backend/routers/api.php
+++ Backend/routers/api.php
@@ -1,3 +1,16 @@
 <?php
 
+use Config\Urls;
+
+$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+$method = $_SERVER['REQUEST_METHOD'];
+
+if (!in_array($request[0], array_column(Urls::cases(),'value'))) {
+    header('HTTP/1.0 404 Not Found', true, 404);
+    $response = ['message' => 'not found'];
+    echo (json_encode($response));
+    die();
+}
+
+
 include __DIR__ . '/products.php';
--- Backend/routers/products.php
+++ Backend/routers/products.php
@@ -4,8 +4,6 @@ use Config\Methods;
 use Config\Urls;
 use Controllers\ProductController;
 
-$method = $_SERVER['REQUEST_METHOD'];
-$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
 
 if (strtolower($method) === strtolower(Methods::GET->value) && !isset($request[1]) && $request[0] === Urls::PRODUCTS->value) {
     $productController = ProductController::getInstance();
